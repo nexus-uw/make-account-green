d3f5791fd74d4914989a11eb4e0bb38cc2ec4fea
simon-ramsay <simon@rangle.io>
2016-05-21 20:57:39 -0400

    added email

--- doWork.sh
+++ doWork.sh
@@ -4,6 +4,7 @@ cd make-account-green
 
 echo $[ 1 + $[ RANDOM % 10 ]] > random_number.txt
 git add random_number.txt
+git config user.email "noop@nowhere.com"
 git config user.name "make account green bot"
 git commit -m 'added new random number'
 git push origin master --follow-tags
