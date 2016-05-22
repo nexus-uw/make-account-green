6a72096d62be930dca3cbf3d3da95755c93549d1
simon-ramsay <simon@rangle.io>
2016-05-21 21:10:29 -0400

    set username

--- doWork.sh
+++ doWork.sh
@@ -5,7 +5,7 @@ cd make-account-green
 echo $[ 1 + $[ RANDOM % 10 ]] > random_number.txt
 git add random_number.txt
 git config user.email "nexus-uw@users.noreply.github.com"
-git config user.name "make work bot"
+git config user.name "nexus-uw"
 git commit -m 'added new random number'
 git push origin master --follow-tags
 cd ..
